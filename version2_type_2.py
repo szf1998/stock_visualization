af4b32f5ee2228bb6d6f7bb1d9a0aa554c17662f
Shi Zhanfei <43202772+szf1998@users.noreply.github.com>
2022-07-15 09:04:33 +0100

    Add files via upload
    add q5 factor

--- version2_type_2.py
+++ version2_type_2.py
@@ -705,13 +705,13 @@ class page4(QMainWindow):
         self.pushButton1.setGeometry(QtCore.QRect(10, 10, 270, 121))
         font1 = QtGui.QFont()
         font1.setFamily("Bradley Hand ITC")
-        font1.setPointSize(18)
+        font1.setPointSize(16)
         self.pushButton1.setFont(font1)
         self.pushButton1.setText("Click to Run\nFama French Model\non Desinated Stock")
         self.pushButton1.clicked.connect(self.pushButton1Clicked)
         # set button2
         self.pushButton_2 = QtWidgets.QPushButton(self)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 210, 250, 61))
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 260, 250, 61))
         font2 = QtGui.QFont()
         font2.setFamily("Bradley Hand ITC")
         font2.setPointSize(18)
@@ -720,16 +720,20 @@ class page4(QMainWindow):
 
         # set figure label
         self.fig_label1 = QLabel(self)
-        self.fig_label1.setGeometry(50, 300, 650, 400)
+        self.fig_label1.setGeometry(50, 330, 650, 450)
         self.fig_label1.setScaledContents(True)  # set this True to let figure fit label size
 
         self.fig_label2 = QLabel(self)
-        self.fig_label2.setGeometry(720, 300, 400, 400)
+        self.fig_label2.setGeometry(720, 330, 400, 450)
         self.fig_label2.setScaledContents(True)  # set this True to let figure fit label size
 
         self.fig_label3 = QLabel(self)
-        self.fig_label3.setGeometry(1140, 300, 400, 400)
+        self.fig_label3.setGeometry(1140, 330, 400, 450)
         self.fig_label3.setScaledContents(True)  # set this True to let figure fit label size
+
+        self.fig_label4 = QLabel(self)
+        self.fig_label4.setGeometry(900, 10, 600, 300)
+        self.fig_label4.setScaledContents(True)  # set this True to let figure fit label size
 
         self.pushButton_2.clicked.connect(self.pushButton2Clicked)
         self.pushButton_2.clicked.connect(self.close)
@@ -741,6 +745,9 @@ class page4(QMainWindow):
         global END_DATE
         stockObject = yf.download(RISKY_ASSET, start=START_DATE, end=END_DATE, group_by="ticker",
                                   proxy="127.0.0.1:33210", index_col=0)
+        # stockObject_qFactor = yf.download(RISKY_ASSET, start=START_DATE, end=END_DATE, group_by="ticker",
+        #                           proxy="127.0.0.1:33210", index_col=0)
+        stockObject_qFactor = stockObject.copy(deep=True)# 复制一份给qfactor用
         returns_3 = pd.read_csv('F-F_Research_Data_Factors_daily.csv', index_col=0)
         returns_5 = pd.read_csv('F-F_Research_Data_5_Factors_2x3_daily.csv', index_col=0)
         returns_3['Date'] = returns_3.index
@@ -815,20 +822,65 @@ class page4(QMainWindow):
                                       stockObject['SMB_5'] + beta_HML_5 * stockObject['HML'] + beta_RMW * stockObject[
                                           'RMW'] + beta_CMA * stockObject['CMA']
 
+        #跑一下q5模型
+        q_returns = pd.read_csv('q5_factors_daily_2021_2.csv', index_col=0)
+        q_returns.columns.tolist
+        # returns.rename_axis('Date',axis='columns')
+        q_returns['Date'] = q_returns.index
+        # print(q_returns)
+        stockObject_qFactor['daily_ret'] = stockObject_qFactor['Close'].pct_change(1)
+
+        stockObject_qFactor['Date'] = stockObject_qFactor.index
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'Mkt-RF', 0)
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'ME', 0)
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'IA', 0)
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'ROE', 0)
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'EG', 0)
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'RF', 0)
+
+        for i in range(q_returns.shape[0]):
+            if Dt.strptime(q_returns['Date'][i], '%Y/%m/%d') == Dt.strptime(
+                    str(stockObject_qFactor['Date'].iloc[0]).split(' ')[0], '%Y-%m-%d'):
+                iloc_offset_qFactor = i
+        print('qFactor OFFSET iteration is', iloc_offset_qFactor)
+        for i in range(stockObject_qFactor.shape[0]):
+            stockObject_qFactor['Mkt-RF'].iloc[i] = q_returns['R_MKT'].iloc[i + iloc_offset_qFactor] - q_returns['R_F'].iloc[
+                i + iloc_offset_qFactor]
+            stockObject_qFactor['ME'].iloc[i] = q_returns['R_ME'].iloc[i + iloc_offset_qFactor]
+            stockObject_qFactor['IA'].iloc[i] = q_returns['R_IA'].iloc[i + iloc_offset_qFactor]
+            stockObject_qFactor['ROE'].iloc[i] = q_returns['R_ROE'].iloc[i + iloc_offset_qFactor]
+            stockObject_qFactor['EG'].iloc[i] = q_returns['R_EG'].iloc[i + iloc_offset_qFactor]
+            stockObject_qFactor['RF'].iloc[i] = q_returns['R_F'].iloc[i + iloc_offset_qFactor]
+            stockObject_qFactor['daily_ret'].iloc[i] -= q_returns['R_F'].iloc[i + iloc_offset_qFactor]
+        # stockObject_qFactor.to_csv('stockObject.csv')
+        x_qf = stockObject_qFactor[['Mkt-RF', 'ME', 'IA', 'ROE', 'EG']].iloc[1:]
+        y_qf = stockObject_qFactor['daily_ret'].iloc[1:]
+        x_qf = sm.add_constant(x_qf)
+        Qf = lm.OLS(y_qf, x_qf).fit()
+        print('q5 factors summary')
+        print(Qf.summary())
+        intercept_q5, beta_Mkt_RF_q5, beta_ME, beta_IA, beta_ROE, beta_EG = Qf.params
+        stockObject_qFactor.insert(stockObject_qFactor.shape[1], 'qf_return', 0)
+        stockObject_qFactor['qf_return'] = intercept_q5 + stockObject_qFactor['RF'] + beta_Mkt_RF_q5 * stockObject_qFactor['Mkt-RF'] + beta_ME * \
+                                   stockObject_qFactor['ME'] + beta_IA * stockObject_qFactor['IA'] + beta_ROE * stockObject_qFactor[
+                                       'ROE'] + beta_EG * stockObject_qFactor['EG']
+
+        #显示ff3、5和q5 收益率与真实收益率
         plt.figure()
         plt.plot(stockObject['daily_ret'], label='Actual daily return')
-        plt.plot(stockObject['ff3fm_return'], label='Fama French 3 factor models daily return')
-        plt.plot(stockObject['ff5fm_return'], label='Fama French 5 factor models daily return')
+        plt.plot(stockObject['ff3fm_return'], label='Fama French 3 factor model daily return')
+        plt.plot(stockObject['ff5fm_return'], label='Fama French 5 factor model daily return')
+        plt.plot(stockObject_qFactor['qf_return'], label='q5 factor model daily return')
         plt.xlabel('Date')
         plt.ylabel('Return Rate')
-        plt.title(f'Comparison of Actual daily return,Fama French 3 factor and 5 factor model return of {RISKY_ASSET}')
+        plt.title(f'Comparison of Actual daily return, Fama French and q5 factor return of {RISKY_ASSET}')
         plt.legend()
-        plt.savefig('FF3&5fm_return.jpg')
+        plt.savefig('FF35&q5fm_return.jpg')
 
-        # 可视化对比一下各个beta系数
+        # 可视化对比一下ff各个beta系数
         plt.figure()
         # plt.bar(['beta_Mkt-RF','beta_SMB','beta_HML'],[beta_Mkt_RF,beta_SMB,beta_HML])
-        plt.bar(['beta_Mkt-RF', 'beta_SMB', 'beta_HML','alpha'],
+        plt.bar(['beta_Mkt-RF', 'beta_SMB', 'beta_HML', 'alpha'],
                 [beta_Mkt_RF_3, beta_SMB_3, beta_HML_3, intercept3])
         plt.xlabel('Coefficient value of Fama French Factors')
         plt.ylabel('Coefficient value')
@@ -843,8 +895,17 @@ class page4(QMainWindow):
         plt.title(f'Beta value of coefficient of Fama French 5 factor of {RISKY_ASSET}')
         plt.savefig('FF5fm_coefficient_value.jpg')
 
+        # 可视化对比一下q5各个beta系数
+        plt.figure()
+        plt.bar(['beta_Mkt-RF', 'beta_ME', 'beta_IA', 'beta_ROE', 'beta_EG', 'alpha'],
+                [beta_Mkt_RF_q5, beta_ME, beta_IA, beta_ROE, beta_EG, intercept_q5])
+        plt.xlabel('Coefficient value of q5 Factors')
+        plt.ylabel('Coefficient value')
+        plt.title(f'Beta value of coefficient of q5 factor of {RISKY_ASSET}')
+        plt.savefig('q5_coefficient_value.jpg')
+
         self.fig_label1.setStyleSheet("border: 1px solid red")  # define fig_label1 in the init_Ui, click button to show
-        self.fig1 = QPixmap('FF3&5fm_return.jpg')
+        self.fig1 = QPixmap('FF35&q5fm_return.jpg')
         self.fig_label1.setPixmap(self.fig1)
         self.fig_label2.setStyleSheet("border: 1px solid red")  # define fig_label1 in the init_Ui, click button to show
         self.fig2 = QPixmap('FF3fm_coefficient_value.jpg')
@@ -852,11 +913,16 @@ class page4(QMainWindow):
         self.fig_label3.setStyleSheet("border: 1px solid red")  # define fig_label1 in the init_Ui, click button to show
         self.fig3 = QPixmap('FF5fm_coefficient_value.jpg')
         self.fig_label3.setPixmap(self.fig3)
+        self.fig_label4.setStyleSheet("border: 1px solid red")  # define fig_label1 in the init_Ui, click button to show
+        self.fig4 = QPixmap('q5_coefficient_value.jpg')
+        self.fig_label4.setPixmap(self.fig4)
 
         self.pushButton1.setText(
             f'Fama French 3 factor coefficients are:\nMkt-RF {round(beta_Mkt_RF_3, 5)},SMB {round(beta_SMB_3, 5)},HML {round(beta_HML_3, 5)}, alpha {round(intercept3, 5)}\n'
             f'Fama French 5 factor coefficients are:\nMkt-RF {round(beta_Mkt_RF_5, 5)},SMB {round(beta_SMB_5, 5)},HML {round(beta_HML_5, 5)}, alpha {round(intercept5, 5)}\n'
-            f'RMW is{round(beta_RMW, 5)},CMA is{round(beta_CMA, 5)}')
+            f'RMW is{round(beta_RMW, 5)},CMA is{round(beta_CMA, 5)}'
+            f'Q5 factor coefficients are:\nMkt-RF {round(beta_Mkt_RF_q5,5)}, ME {round(beta_ME,5)}, IA {round(beta_IA,5)}\n'
+            f'ROE {beta_ROE}, EG {beta_EG}')
         self.pushButton1.adjustSize()
 
     def pushButton2Clicked(self):
@@ -872,6 +938,9 @@ class page4(QMainWindow):
         self.fig_label3.setStyleSheet("")
         self.fig3 = QPixmap('')
         self.fig_label3.setPixmap(self.fig3)
+        self.fig_label4.setStyleSheet("")
+        self.fig4 = QPixmap('')
+        self.fig_label4.setPixmap(self.fig4)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
